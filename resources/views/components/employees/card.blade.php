c9b702c16c5023c9a94f85ac46516dd10ff9f9dc
Alex Hort-Francis <ajhf@hotmail.co.uk>
2023-12-14 14:26:41 +0000

    Add card for employee Show route
    - Add a card that displays information for a single employee.

---
+++ resources/views/components/employees/card.blade.php
@@ -0,0 +1,35 @@
+<div class="bg-white p-10 rounded ">
+    <div class="flex flex-col lg:flex-row gap-10 items-center lg:justify-between p-6   ">
+
+        <div class="flex flex-col md:flex-row gap-10 items-center md:justify-between">
+
+            {{-- Employee name --}}
+            <h2 class="font-bold text-2xl">{{ $employee->first_name }} {{ $employee->last_name }}</h2>
+        </div>
+
+        <div class="flex flex-col">
+                {{-- Employee email --}}
+                <span class="text-slate-500 mr-1">Email:</span> <strong> <a
+                        href="mailto:{{ $employee->email }}">{{ $employee->email }}</a></strong>
+            </span>
+            <span class="text-gray-700 text-base mb-2">
+                {{-- Employee phone number --}}
+                <span class="text-slate-500 mr-1">Phone:</span> <strong>{{ $employee->phone }}</strong>
+            </span>
+            <span class="text-gray-700 text-base mb-2">
+                {{-- Employee company --}}
+                <span class="text-slate-500 mr-1">Company:</span> <strong> <a
+                        href="/companies/{{ $employee->company->id }}">{{ $employee->company->name }}</a></strong>
+            </span>
+        </div>
+
+
+
+
+        <div class="flex space-x-4">
+            {{-- CRUD buttons for employee --}}
+            <x-crud-buttons.update for="employee" :id="$employee->id" />
+            <x-crud-buttons.delete for="employee" :id="$employee->id" />
+        </div>
+    </div>
+</div>
